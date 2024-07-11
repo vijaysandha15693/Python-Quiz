8d6a0d3bb17c30989064cfe15d815abab6035185
vijaysandha15693 <113205362+vijaysandha15693@users.noreply.github.com>
2024-07-11 21:21:45 +0530

    rules.py

---
+++ rules.py
@@ -0,0 +1,9 @@
+print('''\n==========RULES==========
+1. Each round consists of 5 random questions. To answer, you must press A/B/C/D (case-insensitive).
+Your final score will be given at the end.
+2. Easy level is based on  python theory.
+3.Medium level is based on loops,ifelse structures .
+4.Difficult level is based on dictionaries,tuple,list,module,file handling.
+5. Each question consists of 1 point. There's no negative point for wrong answers.
+
+    ''')
