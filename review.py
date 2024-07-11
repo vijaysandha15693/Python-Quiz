ab13454231cbcaaac061885f262b6de2f7bcc9d0
vijaysandha15693 <113205362+vijaysandha15693@users.noreply.github.com>
2024-07-11 21:15:20 +0530

    review.py

---
+++ review.py
@@ -0,0 +1,25 @@
+print('''1.excellent
+2.very good
+3.good
+4.bad
+5.worst''')
+choice = int(input('ENTER YOUR CHOICE: '))
+if choice == 1:
+     print('Excellent')
+     print('============THANKYOU FOR YOUR REVIEW============')
+elif choice == 2:
+     print(' Very Good')
+     print('============THANKYOU FOR YOUR REVIEW============')
+elif choice == 3:
+     print('Good')
+     print('============THANKYOU FOR YOUR REVIEW============')
+elif choice == 4:
+     print('Bad')
+     print('============THANKYOU FOR YOUR REVIEW============')
+elif choice == 5:
+     print('Worst')
+     print('============THANKYOU FOR YOUR REVIEW============')
+else:
+     print('WRONG INPUT. ENTER THE CHOICE AGAIN')
+
+    
